f34acd2b65e957f94693e7724c57a4a0e42d51e7
liupu <575866447@qq.com>
2019-09-12 15:58:27 +0800

    添加部分注释

--- buildSrc/src/main/groovy/com/update/plugin/aop/AopPlugin.groovy
+++ buildSrc/src/main/groovy/com/update/plugin/aop/AopPlugin.groovy
@@ -15,18 +15,21 @@ class AopPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
 
-        println 'project(' + project.name + ') apply aop plugin'
-        project.extensions.create(AopExtension.EXT_NAME, AopExtension)
-        project.afterEvaluate {
-            init(project)//此处要先于transformImpl.transform方法执行
+        // 判断是否为 App
+        def isApp = project.plugins.hasPlugin(AppPlugin)
+        if (isApp) {
+            println 'project(' + project.name + ') apply aop plugin'
+            // 读取自定义配置
+            project.extensions.create(AopExtension.EXT_NAME, AopExtension)
+            project.afterEvaluate {
+                init(project)//此处要先于transformImpl.transform方法执行
+            }
         }
     }
 
-    void init(Project project) {
+    static void init(Project project) {
         AopExtension extension = project.extensions.findByName(AopExtension.EXT_NAME) as AopExtension
         project.logger.error AopExtension.EXT_NAME + ' configuration is:\n' + extension.toString()
-
         project.logger.error extension.aopClass
-
     }
 }
