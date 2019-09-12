300dd84c5e59fb4d289dd2d8b9c02e2fc5efdbeb
liupu <575866447@qq.com>
2019-09-12 12:42:45 +0800

    init

---
+++ buildSrc/src/main/groovy/com/update/plugin/HelloPlugin.groovy
@@ -0,0 +1,15 @@
+package com.update.plugin
+
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+class HelloPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        project.task("testTask"){
+            println "Hello Gradle"
+        }
+    }
+}
