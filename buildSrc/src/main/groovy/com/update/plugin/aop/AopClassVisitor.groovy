d6f853f639efac404621c2a9f3a5ee2c957eab0e
liupu <575866447@qq.com>
2019-09-16 22:27:31 +0800

    AopClassVisitor 补全

--- buildSrc/src/main/groovy/com/update/plugin/aop/AopClassVisitor.groovy
+++ buildSrc/src/main/groovy/com/update/plugin/aop/AopClassVisitor.groovy
@@ -31,6 +31,17 @@ class AopClassVisitor extends ClassVisitor {
     @Override
     MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
         MethodVisitor mv = super.visitMethod(access, name, desc, signature, exceptions)
+        if (shouldProcessMethod(name)) {
+            println('aop method:' + name + desc)
+            mv = new AopMethodVisitor(Opcodes.ASM5, mv, access, name, desc, signature, exceptions, className);
+        }
         return mv
+    }
+
+
+    static boolean shouldProcessMethod(String name) {
+        if ("<init>".equals(name) || "<clinit>".equals(name) || name.startsWith('access$'))
+            return false
+        return true
     }
 }
