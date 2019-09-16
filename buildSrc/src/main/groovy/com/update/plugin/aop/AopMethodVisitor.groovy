69de0f1f71211a2bbcca56e775e09750ac34dae5
liupu <575866447@qq.com>
2019-09-16 22:36:50 +0800

    AopMethodVisitor 完善

--- buildSrc/src/main/groovy/com/update/plugin/aop/AopMethodVisitor.groovy
+++ buildSrc/src/main/groovy/com/update/plugin/aop/AopMethodVisitor.groovy
@@ -11,8 +11,24 @@ import org.objectweb.asm.commons.LocalVariablesSorter
  * github : https://github.com/CodeLiuPu/
  */
 class AopMethodVisitor extends LocalVariablesSorter {
+    int access
+    String name
+    String desc
+    String signature
+    String className
+    String[] exceptions
+    int aopVar
 
-    AopMethodVisitor(int i, String s, MethodVisitor methodVisitor) {
-        super(i, s, methodVisitor)
+    AopMethodVisitor(final int api, final MethodVisitor mv,
+                     int access, String name,
+                     String desc, String signature,
+                     String[] exceptions, String className) {
+        super(api, access, desc, mv)
+        this.access = access
+        this.name = name
+        this.desc = desc
+        this.signature = signature
+        this.exceptions = exceptions
+        this.className = className
     }
 }
